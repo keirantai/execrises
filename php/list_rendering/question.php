84d61b9027be3f8f4514ee3198d703d768885d5b
Keiran Tai <tkeiran@gmail.com>
2017-09-04 18:18:13 +0800

    Corrected grammar in question

--- php/list_rendering/question.php
+++ php/list_rendering/question.php
@@ -1,11 +1,11 @@
 <?php
 
-$item_per_buckets = 7; // number of items render in a bucket
+$items_per_bucket = 7; // number of items render in a bucket
 $number_of_buckets = 5; // number of buckets to render the items
 
-$total_num_links = 26; // total number of items
+$total_num_items = 26; // total number of items
 $list = []; // list of items to render
-for ($i=0; $i < $total_num_links; $i++) {
+for ($i=0; $i < $total_num_items; $i++) {
 	$index = $i+1;
 	array_push($list, "Item {$index}");
 } // generate sample items in the list
