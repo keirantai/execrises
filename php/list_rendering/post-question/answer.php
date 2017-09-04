748139c5c968ca0b71e15c28041a5a1926adc176
Keiran Tai <tkeiran@gmail.com>
2017-09-04 18:16:16 +0800

    Corrected some grammar and align the terms in variable.

--- php/list_rendering/post-question/answer.php
+++ php/list_rendering/post-question/answer.php
@@ -1,11 +1,11 @@
 <?php
 
-$item_per_buckets = 7; // number of items render in a bucket
+$items_per_bucket = 5; // number of items render in a bucket
 $number_of_buckets = 5; // number of buckets to render the items
 
-$total_num_links = 26; // total number of items
+$total_num_items = 26; // total number of items
 $list = []; // list of items to render
-for ($i=0; $i < $total_num_links; $i++) {
+for ($i=0; $i < $total_num_items; $i++) {
 	$index = $i+1;
 	array_push($list, "Item {$index}");
 } // generate sample items in the list
@@ -26,16 +26,16 @@ function ps() {
 }
 
 function main() {
-	global $total_num_links, $list, $number_of_buckets, $item_per_buckets;
+	global $total_num_items, $list, $number_of_buckets, $items_per_bucket;
 
-	$max_number_of_links = min($total_num_links, $item_per_buckets * $number_of_buckets);
+	$max_number_of_links = min($total_num_items, $items_per_bucket * $number_of_buckets);
 
 	for($i=0; $i < $max_number_of_links; $i++) {
 		pl ($list[$i]);
 
 		// check if need a separator
 		for ($col=1; $col <= $number_of_buckets; $col++) {
-			$index_where_to_break = $col * $item_per_buckets;
+			$index_where_to_break = $col * $items_per_bucket;
 			$current_index_from_1 = $i + 1;
 			$is_the_last_item = $current_index_from_1 == $max_number_of_links;
 			if ($current_index_from_1 == $index_where_to_break) {
