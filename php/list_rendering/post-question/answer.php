cb9e7abc1db313390cbec7d29e097d02ebde9b6f
Keiran Tai <tkeiran@gmail.com>
2017-09-04 17:55:24 +0800

    Added a PHP question - list_rendering

---
+++ php/list_rendering/post-question/answer.php
@@ -0,0 +1,50 @@
+<?php
+
+$item_per_buckets = 7; // number of items render in a bucket
+$number_of_buckets = 5; // number of buckets to render the items
+
+$total_num_links = 26; // total number of items
+$list = []; // list of items to render
+for ($i=0; $i < $total_num_links; $i++) {
+	$index = $i+1;
+	array_push($list, "Item {$index}");
+} // generate sample items in the list
+
+/**
+ * Print texts in a line
+ * @param  [type] $text [description]
+ */
+function pl($text) {
+	echo "| $text |\r\n";
+}
+
+/**
+ * Print a separator
+ */
+function ps() {
+	echo "-----------\r\n";
+}
+
+function main() {
+	global $total_num_links, $list, $number_of_buckets, $item_per_buckets;
+
+	$max_number_of_links = min($total_num_links, $item_per_buckets * $number_of_buckets);
+
+	for($i=0; $i < $max_number_of_links; $i++) {
+		pl ($list[$i]);
+
+		// check if need a separator
+		for ($col=1; $col <= $number_of_buckets; $col++) {
+			$index_where_to_break = $col * $item_per_buckets;
+			$current_index_from_1 = $i + 1;
+			$is_the_last_item = $current_index_from_1 == $max_number_of_links;
+			if ($current_index_from_1 == $index_where_to_break) {
+				if (!$is_the_last_item) {
+					ps();
+				}
+			}
+		}
+	}
+}
+
+main();
---
+++ php/list_rendering/question.php
@@ -0,0 +1,32 @@
+<?php
+
+$item_per_buckets = 7; // number of items render in a bucket
+$number_of_buckets = 5; // number of buckets to render the items
+
+$total_num_links = 26; // total number of items
+$list = []; // list of items to render
+for ($i=0; $i < $total_num_links; $i++) {
+	$index = $i+1;
+	array_push($list, "Item {$index}");
+} // generate sample items in the list
+
+/**
+ * Print texts in a line
+ * @param  [type] $text [description]
+ */
+function pl($text) {
+	echo "| $text |\r\n";
+}
+
+/**
+ * Print a separator
+ */
+function ps() {
+	echo "-----------\r\n";
+}
+
+function main() {
+	/** put your code here */
+}
+
+main();
